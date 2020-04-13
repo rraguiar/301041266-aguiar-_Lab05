c0fc9c6f0dde156f68757134f766819dc85f56ca
Rafael Aguiar <rraguiar@yahoo.com>
2020-04-12 20:49:04 -0400

    Implemented Parallel class to Resize and Save the pictures from Flickr Search.

--- FlickrViewer/FlickrViewer/FickrViewerForm.Designer.cs
+++ FlickrViewer/FlickrViewer/FickrViewerForm.Designer.cs
@@ -33,16 +33,19 @@
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.searchButton = new System.Windows.Forms.Button();
             this.imagesListBox = new System.Windows.Forms.ListBox();
+            this.btnResizeSave = new System.Windows.Forms.Button();
+            this.cBoxSizes = new System.Windows.Forms.ComboBox();
+            this.lblComboBox = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 25);
+            this.label1.Location = new System.Drawing.Point(16, 20);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(216, 20);
+            this.label1.Size = new System.Drawing.Size(194, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter Flickr search tags here:";
             // 
@@ -50,10 +53,10 @@
             // 
             this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.inputTextBox.Location = new System.Drawing.Point(244, 20);
-            this.inputTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.inputTextBox.Location = new System.Drawing.Point(217, 16);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(640, 26);
+            this.inputTextBox.Size = new System.Drawing.Size(570, 22);
             this.inputTextBox.TabIndex = 1;
             // 
             // pictureBox
@@ -61,10 +64,10 @@
             this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox.Location = new System.Drawing.Point(244, 56);
-            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox.Location = new System.Drawing.Point(217, 49);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(754, 664);
+            this.pictureBox.Size = new System.Drawing.Size(671, 644);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox.TabIndex = 3;
             this.pictureBox.TabStop = false;
@@ -72,10 +75,10 @@
             // searchButton
             // 
             this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.searchButton.Location = new System.Drawing.Point(896, 17);
-            this.searchButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.searchButton.Location = new System.Drawing.Point(795, 13);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(4);
             this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(105, 35);
+            this.searchButton.Size = new System.Drawing.Size(93, 28);
             this.searchButton.TabIndex = 4;
             this.searchButton.Text = "Search";
             this.searchButton.UseVisualStyleBackColor = true;
@@ -84,26 +87,59 @@
             // imagesListBox
             // 
             this.imagesListBox.FormattingEnabled = true;
-            this.imagesListBox.ItemHeight = 20;
-            this.imagesListBox.Location = new System.Drawing.Point(22, 56);
-            this.imagesListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.imagesListBox.ItemHeight = 16;
+            this.imagesListBox.Location = new System.Drawing.Point(20, 49);
+            this.imagesListBox.Margin = new System.Windows.Forms.Padding(4);
             this.imagesListBox.Name = "imagesListBox";
-            this.imagesListBox.Size = new System.Drawing.Size(211, 664);
+            this.imagesListBox.Size = new System.Drawing.Size(188, 644);
             this.imagesListBox.TabIndex = 5;
             this.imagesListBox.SelectedIndexChanged += new System.EventHandler(this.imagesListBox_SelectedIndexChanged);
+            // 
+            // btnResizeSave
+            // 
+            this.btnResizeSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResizeSave.Location = new System.Drawing.Point(895, 13);
+            this.btnResizeSave.Name = "btnResizeSave";
+            this.btnResizeSave.Size = new System.Drawing.Size(164, 60);
+            this.btnResizeSave.TabIndex = 6;
+            this.btnResizeSave.Text = "Resize and Save";
+            this.btnResizeSave.UseVisualStyleBackColor = true;
+            this.btnResizeSave.Click += new System.EventHandler(this.btnResizeSave_Click);
+            // 
+            // cBoxSizes
+            // 
+            this.cBoxSizes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cBoxSizes.FormattingEnabled = true;
+            this.cBoxSizes.Location = new System.Drawing.Point(895, 117);
+            this.cBoxSizes.Name = "cBoxSizes";
+            this.cBoxSizes.Size = new System.Drawing.Size(164, 24);
+            this.cBoxSizes.TabIndex = 7;
+            // 
+            // lblComboBox
+            // 
+            this.lblComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblComboBox.Location = new System.Drawing.Point(895, 85);
+            this.lblComboBox.Name = "lblComboBox";
+            this.lblComboBox.Size = new System.Drawing.Size(167, 29);
+            this.lblComboBox.TabIndex = 8;
+            this.lblComboBox.Text = "Choose Size to Save";
+            this.lblComboBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FickrViewerForm
             // 
             this.AcceptButton = this.searchButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1018, 734);
+            this.ClientSize = new System.Drawing.Size(1081, 715);
             this.Controls.Add(this.imagesListBox);
             this.Controls.Add(this.searchButton);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Controls.Add(this.btnResizeSave);
+            this.Controls.Add(this.lblComboBox);
+            this.Controls.Add(this.cBoxSizes);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FickrViewerForm";
             this.Text = "Flickr Viewer";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
@@ -119,6 +155,9 @@
       private System.Windows.Forms.PictureBox pictureBox;
       private System.Windows.Forms.Button searchButton;
       private System.Windows.Forms.ListBox imagesListBox;
-   }
+        private System.Windows.Forms.Button btnResizeSave;
+        private System.Windows.Forms.ComboBox cBoxSizes;
+        private System.Windows.Forms.Label lblComboBox;
+    }
 }
 
--- FlickrViewer/FlickrViewer/FickrViewerForm.cs
+++ FlickrViewer/FlickrViewer/FickrViewerForm.cs
@@ -1,6 +1,7 @@
 ﻿// Fig. 23.4: FickrViewerForm.cs
 // Invoking a web service asynchronously with class HttpClient
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -11,111 +12,177 @@ using System.Xml.Linq;
 
 namespace FlickrViewer
 {
-   public partial class FickrViewerForm : Form
-   {
-      // Use your Flickr API key here--you can get one at:
-      // https://www.flickr.com/services/apps/create/apply
-      private const string KEY = "2e77003f5a5ab4e50baf9fcedfc5ee0c";
+    public partial class FickrViewerForm : Form
+    {
 
-      // object used to invoke Flickr web service      
-      private static HttpClient flickrClient = new HttpClient();
+        // Use your Flickr API key here--you can get one at:
+        // https://www.flickr.com/services/apps/create/apply
+        private const string KEY = "2e77003f5a5ab4e50baf9fcedfc5ee0c";
 
-      Task<string> flickrTask = null; // Task<string> that queries Flickr
+        // object used to invoke Flickr web service      
+        private static HttpClient flickrClient = new HttpClient();
 
-      public FickrViewerForm()
-      {
-         InitializeComponent();
-      }
+        Task<string> flickrTask = null; // Task<string> that queries Flickr
 
-      // initiate asynchronous Flickr search query; 
-      // display results when query completes
-      private async void searchButton_Click(object sender, EventArgs e)
-      {
-         // if flickrTask already running, prompt user 
-         if (flickrTask?.Status != TaskStatus.RanToCompletion)
-         {
-            var result = MessageBox.Show(
-               "Cancel the current Flickr search?",
-               "Are you sure?", MessageBoxButtons.YesNo,
-               MessageBoxIcon.Question);
+        //Make FlickrPhotos a global variable
+        //var FlickrPhotos;
+        IEnumerable<FlickrResult> flickrPhotos;
+        byte[] imageToSave;
 
-            // determine whether user wants to cancel prior search
-            if (result == DialogResult.No)
+        int imageNumber = 0;
+
+        public FickrViewerForm()
+        {
+            InitializeComponent();
+
+            //Initialize ComboBox options
+            cBoxSizes.Items.AddRange(new object[] { "Half Size", "Full Size" });
+            cBoxSizes.SelectedIndex = 0;
+
+        }
+
+        // initiate asynchronous Flickr search query; 
+        // display results when query completes
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            // if flickrTask already running, prompt user 
+            if (flickrTask?.Status != TaskStatus.RanToCompletion)
             {
-               return;
+                var result = MessageBox.Show(
+                   "Cancel the current Flickr search?",
+                   "Are you sure?", MessageBoxButtons.YesNo,
+                   MessageBoxIcon.Question);
+
+                // determine whether user wants to cancel prior search
+                if (result == DialogResult.No)
+                {
+                    return;
+                }
+                else
+                {
+                    flickrClient.CancelPendingRequests(); // cancel search
+                }
             }
-            else
+
+            // Flickr's web service URL for searches                         
+            var flickrURL = "https://api.flickr.com/services/rest/?method=" +
+               $"flickr.photos.search&api_key={KEY}&" +
+               $"tags={inputTextBox.Text.Replace(" ", ",")}" +
+               "&tag_mode=all&per_page=500&privacy_filter=1";
+
+            imagesListBox.DataSource = null; // remove prior data source
+            imagesListBox.Items.Clear(); // clear imagesListBox
+            pictureBox.Image = null; // clear pictureBox
+            imagesListBox.Items.Add("Loading..."); // display Loading...
+
+            // invoke Flickr web service to search Flick with user's tags
+            flickrTask = flickrClient.GetStringAsync(flickrURL);
+
+            // await flickrTask then parse results with XDocument and LINQ
+            XDocument flickrXML = XDocument.Parse(await flickrTask);
+
+            // gather information on all photos
+            flickrPhotos =
+               from photo in flickrXML.Descendants("photo")
+               let id = photo.Attribute("id").Value
+               let title = photo.Attribute("title").Value
+               let secret = photo.Attribute("secret").Value
+               let server = photo.Attribute("server").Value
+               let farm = photo.Attribute("farm").Value
+               select new FlickrResult
+               {
+                   Title = title,
+                   URL = $"https://farm{farm}.staticflickr.com/" +
+                     $"{server}/{id}_{secret}.jpg"
+               };
+            imagesListBox.Items.Clear(); // clear imagesListBox
+
+            // set ListBox properties only if results were found
+            if (flickrPhotos.Any())
             {
-               flickrClient.CancelPendingRequests(); // cancel search
+                imagesListBox.DataSource = flickrPhotos.ToList();
+                imagesListBox.DisplayMember = "Title";
             }
-         }
-
-         // Flickr's web service URL for searches                         
-         var flickrURL = "https://api.flickr.com/services/rest/?method=" +
-            $"flickr.photos.search&api_key={KEY}&" +
-            $"tags={inputTextBox.Text.Replace(" ", ",")}" +
-            "&tag_mode=all&per_page=500&privacy_filter=1";
-
-         imagesListBox.DataSource = null; // remove prior data source
-         imagesListBox.Items.Clear(); // clear imagesListBox
-         pictureBox.Image = null; // clear pictureBox
-         imagesListBox.Items.Add("Loading..."); // display Loading...
-
-         // invoke Flickr web service to search Flick with user's tags
-         flickrTask = flickrClient.GetStringAsync(flickrURL);
-
-         // await flickrTask then parse results with XDocument and LINQ
-         XDocument flickrXML = XDocument.Parse(await flickrTask);
-
-         // gather information on all photos
-         var flickrPhotos =
-            from photo in flickrXML.Descendants("photo")
-            let id = photo.Attribute("id").Value
-            let title = photo.Attribute("title").Value
-            let secret = photo.Attribute("secret").Value
-            let server = photo.Attribute("server").Value
-            let farm = photo.Attribute("farm").Value
-            select new FlickrResult
+            else // no matches were found
             {
-               Title = title,
-               URL = $"https://farm{farm}.staticflickr.com/" +
-                  $"{server}/{id}_{secret}.jpg"
-            };
-         imagesListBox.Items.Clear(); // clear imagesListBox
-
-         // set ListBox properties only if results were found
-         if (flickrPhotos.Any())
-         {
-            imagesListBox.DataSource = flickrPhotos.ToList();
-            imagesListBox.DisplayMember = "Title";
-         }
-         else // no matches were found
-         {
-            imagesListBox.Items.Add("No matches");
-         }
-      }
-
-      // display selected image
-      private async void imagesListBox_SelectedIndexChanged(
-         object sender, EventArgs e)
-      {
-         if (imagesListBox.SelectedItem != null)
-         {
-            string selectedURL = ((FlickrResult) imagesListBox.SelectedItem).URL;
-
-            // use HttpClient to get selected image's bytes asynchronously
-            byte[] imageBytes = await flickrClient.GetByteArrayAsync(selectedURL);
-
-            // display downloaded image in pictureBox                  
-            using (var memoryStream = new MemoryStream(imageBytes))
-            {
-               pictureBox.Image = Image.FromStream(memoryStream);
+                imagesListBox.Items.Add("No matches");
             }
-         }
-      }
-   }
+        }
+
+        // display selected image
+        private async void imagesListBox_SelectedIndexChanged(
+           object sender, EventArgs e)
+        {
+            if (imagesListBox.SelectedItem != null)
+            {
+                string selectedURL = ((FlickrResult)imagesListBox.SelectedItem).URL;
+
+                try
+                {
+
+                    // use HttpClient to get selected image's bytes asynchronously
+                    byte[] imageBytes = await flickrClient.GetByteArrayAsync(selectedURL);
+
+                    // display downloaded image in pictureBox                  
+                    using (var memoryStream = new MemoryStream(imageBytes))
+                    {
+                        pictureBox.Image = Image.FromStream(memoryStream);
+                    }
+                }
+                //I don´t want to do anything at this point, just avoid from the app breaking.
+                catch { }
+            }
+        }
+        /// <summary>
+        /// Method to handle the resize and save button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnResizeSave_Click(object sender, EventArgs e)
+        {
+            int comboBoxSelection = cBoxSizes.SelectedIndex;
+            try
+            {
+                if (flickrPhotos.Any())
+                {
+                    string imageName = "picture";
+                    Parallel.ForEach(flickrPhotos, (image) => saveImage(comboBoxSelection, imageName, image));
+                }
+                else
+                {
+                    MessageBox.Show("No image found to Save.");
+                }
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Method that resize the picture accordingly to the option selected on the Combo Box
+        /// and save it as a ".png" file.
+        /// </summary>
+        /// <param name="comboBoxIndex"></param>
+        /// <param name="imageName"></param>
+        /// <param name="image"></param>
+        private void saveImage(int comboBoxIndex, string imageName, FlickrResult image)
+        {
+            try
+            {
+                imageToSave = (Task.Run(() => flickrClient.GetByteArrayAsync(image.URL)).Result);
+                MemoryStream memoryStream = new MemoryStream(imageToSave);
+                Image img = Image.FromStream(memoryStream);
+                imageNumber++;
+                //if comboBox is at index position 0, Half Size images, otherwise it saves on the original size
+                if (comboBoxIndex == 0)
+                {
+                    img = img.GetThumbnailImage((int)(img.Width / 2), (int)(img.Height / 2), null, IntPtr.Zero);
+                }
+                //Uses the SAVE method from Image class and save the files into the \bin\Debug
+                img.Save((imageName + imageNumber + ".png"), System.Drawing.Imaging.ImageFormat.Png);
+            }
+            catch { }
+        }
+    }
 }
-
 /**************************************************************************
  * (C) Copyright 1992-2017 by Deitel & Associates, Inc. and               *
  * Pearson Education, Inc. All Rights Reserved.                           *
